841f3536f06bdba2a9df5c4b44970b104d1fed56
Jayleh <jaylehyun@gmail.com>
2018-02-03 10:00:34 -0800

    working on pybank

--- Homework3/PyBank/main.py
+++ Homework3/PyBank/main.py
@@ -8,13 +8,28 @@ with open(csv_path, 'r', newline='', encoding='utf-8') as csv_file:
 
     budget_data = csv.DictReader(csv_file, delimiter=',')
 
+    months = []
+    revenue = []
     total_months = 0
     total_revenue = 0
 
     for line in budget_data:
+        # Count months
         total_months += 1
+
+        # Append dates into list
+        months.append(line['Date'])
+
+        # Format revenue
         line['Revenue'] = int(line['Revenue'])
+
+        # Append revenue into list
+        revenue.append(line['Revenue'])
+
+        # Add to total revenue
         total_revenue = total_revenue + line['Revenue']
 
+    print(len(months))
+    print(sum(revenue))
     print("Total Months: " + str(total_months))
     print("Total Revenue: " + str(total_revenue))
