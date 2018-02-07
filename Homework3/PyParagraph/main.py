5b1314bebbedacca9ca0a1ac44381ea0e60bb5ae
Jayleh <jaylehyun@gmail.com>
2018-02-07 13:08:49 -0800

    found approximate word and sentence count

--- Homework3/PyParagraph/main.py
+++ Homework3/PyParagraph/main.py
@@ -7,7 +7,21 @@ txt_path = os.path.join(os.getcwd(), 'PyParagraph\\0 raw_data', 'paragraph_1.txt
 
 with open(txt_path, 'r') as input_txt:
 
-    # Read csv file as data frame
+    # Read text file
     para = input_txt.read()
 
+    # Split paragraph by spaces
+    para = para.split(' ')
+
+    # Declare variables
+    word_count = len(para)
+    sentence_count = 0
+
+    for word in para:
+        if '.' in word:
+            sentence_count += 1
+
     print(para)
+    print('Paragraph Analysis\n-----------------')
+    print(f'Approximate Word Count: {word_count}')
+    print(f'Approximate Sentence Count: {sentence_count}')
