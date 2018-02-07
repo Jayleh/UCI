900dc04c8fc36e2392baad9cb90156d0ed274c29
Jayleh <jaylehyun@gmail.com>
2018-02-06 18:31:47 -0800

    put names into lists to zip

--- Homework3/PyBoss/main.py
+++ Homework3/PyBoss/main.py
@@ -48,6 +48,8 @@ with open(csv_path, 'r', newline='', encoding='utf-8') as input_csv:
     csv_reader = csv.DictReader(input_csv, delimiter=',')
 
     emp_data = list(csv_reader)
+    first_name = []
+    last_name = []
 
     for emp in emp_data:
         emp['Name'] = split_name(emp['Name'])
@@ -55,25 +57,37 @@ with open(csv_path, 'r', newline='', encoding='utf-8') as input_csv:
         emp['SSN'] = parse_ssn(emp['SSN'])
         emp['State'] = parse_state(emp['State'])
 
+        # Append first names to list
+        first_name.append(emp['Name'][0])
+
         # Create new column with first name
-        first_name = emp['Name'][0]
-        emp['First Name'] = first_name
+        # first_name = emp['Name'][0]
+        # emp['First Name'] = first_name
+
+        # Append last names to list
+        last_name.append(emp['Name'][1])
+
+        # Rename Name to First Name
+        emp['First Name'] = emp.pop('Name')
 
         # Create new column with last name
-        last_name = emp['Name'][1]
-        emp['Last Name'] = last_name
+        # last_name = emp['Name'][1]
+        # emp['Last Name'] = last_name
 
     print(emp_data[0])
+    print(first_name[0:4])
+    print(last_name[0:4])
 
     # Declare new csv file path
     new_csv_path = os.path.join(os.getcwd(), 'PyBoss\\Employee Data', 'employee_data1.csv')
 
-#     with open(new_csv_path, 'w') as output_csv:
-#         # Fieldnames
-#         headers = ['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State']
-#
-#         csv_writer = csv.DictWriter(output_csv, fieldnames=headers, delimiter=',')
-#
-#         for column in emp_data:
-#             writer.writerows((column['Emp ID'], column['First Name'],
-#                             column['Last Name'], column['DOB'], column['SSN'], column['State'])
+    with open(new_csv_path, 'w') as output_csv:
+        # Fieldnames
+        headers = ['Emp ID', 'First Name', 'Last Name', 'DOB', 'SSN', 'State']
+        csv_writer = csv.DictWriter(output_csv, fieldnames=headers, delimiter=',')
+
+        # for row in emp_data:
+        #     csv_writer.writerow(row)
+
+        # for column in emp_data:
+        #    csv_writer.writerow(column['Emp ID'], column['First Name'], column['Last Name'], column['DOB'], column['SSN'], column['State'])
