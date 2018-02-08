518df7052fca368dde5219e30256a6311dfb1595
Jayleh <jaylehyun@gmail.com>
2018-02-07 21:51:49 -0800

    PyParagraph Complete

--- Homework3/PyParagraph/main.py
+++ Homework3/PyParagraph/main.py
@@ -34,14 +34,41 @@ with open(txt_path, 'r') as input_txt:
         elif '?' in word:
             sentence_count += 1
 
+    # Create list for word count for each sentence
     word_count_sent = []
+
     count = 0
     for word in para_no_space:
+        # Append length of each word into list
         letter_count_word.append(len(word))
 
-    print(para_no_space)
+        # Increment count for each word
+        count += 1
+
+        # Loop to reset count at end of sentence
+        if '.' in word:
+            # Append word count of each sentence into list
+            word_count_sent.append(count)
+
+            # Reset count
+            count = 0
+
+    # print(para_no_space)
+    # print(word_count_sent)
     print('Paragraph Analysis\n-----------------')
     print(f'Approximate Word Count: {word_count}')
     print(f'Approximate Sentence Count: {sentence_count}')
     print(f'Average Letter Count: {round(mean(letter_count_word), 5)}')
-    print('Average Sentence Length: ')
+    print(f'Average Sentence Length: {mean(word_count_sent)}')
+
+    # Declare text file path
+    txt_path = os.path.join(
+        os.getcwd(), 'PyParagraph\\Paragraph Analysis', 'paragraph_1.txt')
+
+    with open(txt_path, 'w') as txt_file:
+
+        txt_file.write('Paragraph Analysis\n-----------------\n')
+        txt_file.write(f'Approximate Word Count: {word_count}\n')
+        txt_file.write(f'Approximate Sentence Count: {sentence_count}\n')
+        txt_file.write(f'Average Letter Count: {round(mean(letter_count_word), 5)}\n')
+        txt_file.write(f'Average Sentence Length: {round(mean(word_count_sent), 1)}')
